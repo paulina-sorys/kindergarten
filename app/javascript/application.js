845b96ea58126bbb571985f2bb2364f1804cedd8
Paulina <paullina.sorys@gmail.com>
2026-02-12 22:15:35 +0100

    Fix scan_js job by adding missing importmap-rails gem

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,1 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-
-// Vue 3
-import { createApp } from 'vue'
-import App from './app.vue'
-
-document.addEventListener('DOMContentLoaded', () => {
-  const app = createApp(App)
-  app.mount('#app')
-})
